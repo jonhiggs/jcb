a2bc01b2de6814f61976b587078387cf7a047de7
Jon Higgs <jhiggs@eml.cc>
2022-11-29 19:51:16 +1100

    Add some more menu movements

--- lib/ui/scan.go
+++ lib/ui/scan.go
@@ -8,14 +8,9 @@ func scanMain() {
 	for {
 		ch := transactionWin.GetChar()
 		switch ch {
-		//case 'W':
-		//	ui.Commit()
-		//	ui.MainWindow.Touch()
-		//	ui.MainWindow.Refresh()
 		//case 'x':
-		//	idStr := uiTransaction.TransactionMenu.Current(nil).Description()
+		//	idStr := transactionMenu.Current(nil).Description()
 		//	id, _ := strconv.ParseInt(idStr, 10, 64)
-
 		//	db.DeleteTransaction(id)
 		//	err := uiTransaction.DeleteTransactionMenuItem(id)
 		//	if err != nil {
@@ -31,36 +26,36 @@ func scanMain() {
 		//	uiTransaction.TransactionWindow.Refresh()
 		//	ui.MainWindow.Touch()
 		//	ui.MainWindow.Refresh()
-		//case 'g':
-		//	uiTransaction.TransactionMenu.Driver(gc.DriverActions[gc.KEY_HOME])
-		//case 'G':
-		//	uiTransaction.TransactionMenu.Driver(gc.DriverActions[gc.KEY_END])
-		//case 'd':
-		//	uiTransaction.TransactionMenu.Driver(gc.DriverActions[gc.KEY_PAGEDOWN])
-		//case 'u':
-		//	uiTransaction.TransactionMenu.Driver(gc.DriverActions[gc.KEY_PAGEUP])
-		//case 'j':
-		//	uiTransaction.TransactionMenu.Driver(gc.DriverActions[gc.KEY_DOWN])
+		case 'g':
+			transactionMenu.Driver(gc.DriverActions[gc.KEY_HOME])
+		case 'G':
+			transactionMenu.Driver(gc.DriverActions[gc.KEY_END])
+		case 'd':
+			transactionMenu.Driver(gc.DriverActions[gc.KEY_PAGEDOWN])
+		case 'u':
+			transactionMenu.Driver(gc.DriverActions[gc.KEY_PAGEUP])
+		case 'j':
+			transactionMenu.Driver(gc.DriverActions[gc.KEY_DOWN])
+		case 'k':
+			transactionMenu.Driver(gc.DriverActions[gc.KEY_UP])
 		//case 'J':
-		//	curItem := uiTransaction.TransactionMenu.Current(nil)
+		//	curItem := transactionMenu.Current(nil)
 		//	curMonth, _ := strconv.ParseInt(curItem.Name()[5:7], 10, 64)
-		//	for _, item := range uiTransaction.TransactionMenu.Items() {
+		//	for _, item := range transactionMenu.Items() {
 		//		thisMonth, _ := strconv.ParseInt(item.Name()[5:7], 10, 64)
 		//		if thisMonth > curMonth {
-		//			uiTransaction.TransactionMenu.Current(item)
+		//			transactionMenu.Current(item)
 		//			ui.MainWindow.MovePrint(0, 0, item.Index())
 		//			break
 		//		}
 		//	}
-		//case 'k':
-		//	uiTransaction.TransactionMenu.Driver(gc.DriverActions[gc.KEY_UP])
 		//case 'K':
-		//	//curDate := uiStringify.Date(uiTransaction.TransactionMenu.Current(nil).Name())
-		//	//items := uiTransaction.TransactionMenu.Items()
+		//	//curDate := uiStringify.Date(transactionMenu.Current(nil).Name())
+		//	//items := transactionMenu.Items()
 		//	//ui.MainWindow.MovePrint(0, 0, thisMonth)
 		//	//ui.MainWindow.MovePrint(1, 0, curMonth)
 		//	//ui.MainWindow.MovePrint(2, 0, i)
-		//	//for i := len(items) - 1; items[i].Index() != uiTransaction.TransactionMenu.Current(nil).Index(); i = i - 1 {
+		//	//for i := len(items) - 1; items[i].Index() != transactionMenu.Current(nil).Index(); i = i - 1 {
 		//	//}
 		case 'i':
 			renderTransactionInsert()
