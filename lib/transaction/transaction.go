5e53ee3a867154d6dfdc3736b78660064126fc93
Jon Higgs <jhiggs@eml.cc>
2022-11-30 07:41:15 +1100

    Add some more validation

--- lib/transaction/transaction.go
+++ lib/transaction/transaction.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"jcb/db"
 	"jcb/domain"
+	"strconv"
 )
 
 func Save(t domain.Transaction) error {
@@ -18,7 +19,35 @@ func All() ([]domain.Transaction, error) {
 	return db.AllTransactions()
 }
 
-func Validate(t domain.Transaction) (domain.Transaction, error) {
-	return t, errors.New("It always fails")
-	//return t, nil
+func Validate(t domain.Transaction) error {
+	var err error
+	err = validateDate(t)
+	if err != nil {
+		return err
+	}
+
+	err = validateAmount(t)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func validateDate(t domain.Transaction) error {
+	year, err := strconv.ParseInt(t.Date.Format("2006"), 10, 64)
+	if err != nil {
+		return errors.New("Unable to parse the year")
+	}
+	if year < 2000 {
+		return errors.New("Invalid Year")
+	}
+	return nil
+}
+
+func validateAmount(t domain.Transaction) error {
+	if t.Cents == 0 {
+		return errors.New("Amount cannot be zero")
+	}
+	return nil
 }
