901db5984258ee6d1fdb1e7521b307eb401a4735
Jon Higgs <jhiggs@eml.cc>
2022-12-01 07:48:26 +1100

    Break up the formatter

--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module jcb
 go 1.18
 
 require (
-	github.com/mattn/go-sqlite3 v1.14.16 // indirect
-	github.com/rthornton128/goncurses v0.0.0-20220628231859-fd57939296e5 // indirect
+	github.com/mattn/go-sqlite3 v1.14.16
+	github.com/rthornton128/goncurses v0.0.0-20220628231859-fd57939296e5
 )
--- lib/ui/formatters.go
+++ lib/ui/formatters.go
@@ -1,8 +1,8 @@
 package ui
 
 import (
-	"fmt"
 	"jcb/domain"
+	sform "jcb/lib/ui/formatter/string"
 	"strconv"
 	"strings"
 	"time"
@@ -17,10 +17,10 @@ type FormattedTransaction struct {
 
 // convert a struct of data to a struct of strings
 func formatTransaction(t domain.Transaction) FormattedTransaction {
-	id := fmt.Sprintf("%d", t.Id)
-	date := t.Date.Format("2006-01-02")
+	id := sform.Id(t.Id)
+	date := sform.Date(t.Date)
 	description := t.Description
-	amount := formatCents(t.Cents)
+	amount := sform.Cents(t.Cents)
 
 	return FormattedTransaction{id, date, description, amount}
 }
@@ -33,21 +33,4 @@ func unformatTransaction(t FormattedTransaction) domain.Transaction {
 	cents, _ := strconv.ParseInt(strings.Replace(strings.Trim(t.Amount, " "), ".", "", 1), 10, 64)
 
 	return domain.Transaction{id, date, description, cents}
-}
-
-func formatCents(i int64) string {
-	s := fmt.Sprintf("%d", i)
-	var d string
-	var c string
-	if len(s) == 2 {
-		d = "0"
-		c = s
-	} else if len(s) == 1 {
-		d = "0"
-		c = fmt.Sprintf("0%s", s)
-	} else {
-		d = s[0 : len(s)-2]
-		c = s[len(s)-2 : len(s)]
-	}
-	return fmt.Sprintf("%s.%s", d, c)
 }
