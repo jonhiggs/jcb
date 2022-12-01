abab4800086be9f7234bc1f2809b59706604eb09
Jon Higgs <jhiggs@eml.cc>
2022-12-01 21:40:39 +1100

    Remove some unused code

--- lib/transaction/transaction.go
+++ lib/transaction/transaction.go
@@ -1,10 +1,8 @@
 package transaction
 
 import (
-	"errors"
 	"jcb/db"
 	"jcb/domain"
-	"strconv"
 )
 
 func Find(id int64) (domain.Transaction, error) {
@@ -21,49 +19,4 @@ func Delete(t domain.Transaction) error {
 
 func All() ([]domain.Transaction, error) {
 	return db.AllTransactions()
-}
-
-func Validate(t domain.Transaction) error {
-	var err error
-	err = validateDate(t)
-	if err != nil {
-		return err
-	}
-
-	err = validateDescription(t)
-	if err != nil {
-		return err
-	}
-
-	err = validateAmount(t)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func validateDate(t domain.Transaction) error {
-	year, err := strconv.ParseInt(t.Date.Format("2006"), 10, 64)
-	if err != nil {
-		return errors.New("Unable to parse the year")
-	}
-	if year < 2000 {
-		return errors.New("Invalid Year")
-	}
-	return nil
-}
-
-func validateDescription(t domain.Transaction) error {
-	if len(t.Description) <= 0 {
-		return errors.New("Description cannot be empty")
-	}
-	return nil
-}
-
-func validateAmount(t domain.Transaction) error {
-	if t.Cents == 0 {
-		return errors.New("Amount cannot be zero")
-	}
-	return nil
 }
