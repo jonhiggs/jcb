d2141c6a2a5383a6b8616997a47bda334c5ddecc
Jon Higgs <jhiggs@eml.cc>
2022-12-02 10:24:41 +1100

    Expand repeated times until an end time

--- ui/repeater/repeater_test.go
+++ ui/repeater/repeater_test.go
@@ -86,9 +86,10 @@ func TestExpandOneDay(t *testing.T) {
 	var err error
 	var got string
 
-	ts := time.Date(2022, 1, 1, 0, 0, 0, 0, time.UTC)
+	startDate := time.Date(2022, 1, 1, 0, 0, 0, 0, time.UTC)
+	endDate := time.Date(2022, 12, 31, 23, 59, 59, 59, time.UTC)
 
-	res, err = Expand(ts, "1d")
+	res, err = Expand(startDate, endDate, "1d")
 	got = res[0].Format("2006-01-02")
 	if got != "2022-01-01" {
 		t.Error(fmt.Sprintf("first element expected 2022-01-01 but was %s", got))
@@ -113,9 +114,10 @@ func TestExpandOneWeek(t *testing.T) {
 	var err error
 	var got string
 
-	ts := time.Date(2022, 1, 1, 0, 0, 0, 0, time.UTC)
+	startDate := time.Date(2022, 1, 1, 0, 0, 0, 0, time.UTC)
+	endDate := time.Date(2022, 12, 31, 23, 59, 59, 59, time.UTC)
 
-	res, err = Expand(ts, "1w")
+	res, err = Expand(startDate, endDate, "1w")
 	got = res[0].Format("2006-01-01")
 	if got != "2022-01-01" {
 		t.Error(fmt.Sprintf("first element expected 2022-01-01 but was %s", got))
@@ -145,9 +147,10 @@ func TestExpandOneMonth(t *testing.T) {
 	var err error
 	var got string
 
-	ts := time.Date(2022, 1, 1, 0, 0, 0, 0, time.UTC)
+	startDate := time.Date(2022, 1, 1, 0, 0, 0, 0, time.UTC)
+	endDate := time.Date(2022, 12, 31, 23, 59, 59, 59, time.UTC)
 
-	res, err = Expand(ts, "1m")
+	res, err = Expand(startDate, endDate, "1m")
 	got = res[0].Format("2006-01-02")
 	if got != "2022-01-01" {
 		t.Error(fmt.Sprintf("first element expected 2022-01-01 but was %s", got))
@@ -177,9 +180,10 @@ func TestExpandOneMonth31(t *testing.T) {
 	var err error
 	var got string
 
-	ts := time.Date(2022, 1, 31, 0, 0, 0, 0, time.UTC)
+	startDate := time.Date(2022, 1, 31, 0, 0, 0, 0, time.UTC)
+	endDate := time.Date(2022, 12, 31, 23, 59, 59, 59, time.UTC)
 
-	res, err = Expand(ts, "1m")
+	res, err = Expand(startDate, endDate, "1m")
 	got = res[0].Format("2006-01-02")
 	if got != "2022-01-31" {
 		t.Error(fmt.Sprintf("first element expected 2022-01-31 but was %s", got))
@@ -197,6 +201,34 @@ func TestExpandOneMonth31(t *testing.T) {
 
 	if len(res) != 12 {
 		t.Error(fmt.Sprintf("expected 12 elements, got %d.", len(res)))
+	}
+
+	if err != nil {
+		t.Error("error was not expected")
+	}
+}
+
+func TestExpandShortEnd(t *testing.T) {
+	var res []time.Time
+	var err error
+	var got string
+
+	startDate := time.Date(2022, 1, 1, 0, 0, 0, 0, time.UTC)
+	endDate := time.Date(2022, 1, 10, 23, 59, 59, 59, time.UTC)
+
+	res, err = Expand(startDate, endDate, "2d")
+	got = res[0].Format("2006-01-02")
+	if got != "2022-01-01" {
+		t.Error(fmt.Sprintf("first element expected 2022-01-01 but was %s", got))
+	}
+
+	got = res[1].Format("2006-01-02")
+	if got != "2022-01-03" {
+		t.Error(fmt.Sprintf("second element expected 2022-01-03 but was %s", got))
+	}
+
+	if len(res) != 5 {
+		t.Error(fmt.Sprintf("expected 5 elements, got %d.", len(res)))
 	}
 
 	if err != nil {
