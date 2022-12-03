6e2f6f464bca4921041dcd47e408277d6a147568
Jon Higgs <jhiggs@eml.cc>
2022-12-03 16:52:23 +1100

    Make the repeater work much better

--- ui/repeater/repeater.go
+++ ui/repeater/repeater.go
@@ -1,7 +1,6 @@
 package repeater
 
 import (
-	"errors"
 	"strconv"
 	"strings"
 	"time"
@@ -9,43 +8,65 @@ import (
 
 func Expand(date time.Time, startDate time.Time, endDate time.Time, rule string) ([]time.Time, error) {
 	if strings.HasPrefix(rule, "0") {
-		if date.Unix() >= startDate.Unix() && date.Unix() < endDate.Unix() {
-			return []time.Time{date}, nil
-		} else {
+
+		if date.Unix() < startDate.Unix() {
 			return []time.Time{}, nil
 		}
-	} else {
-		timestamps := []time.Time{date}
-		for date.Unix() >= startDate.Unix() && date.Unix() < endDate.Unix() {
-			date, err := relativeDate(date, rule)
-			if err != nil {
-				return timestamps, err
-			}
-			timestamps = append(timestamps, date)
-		}
-		return timestamps, nil
+
+		t, err := relativeDate(date, rule, 1)
+		return []time.Time{t}, err
 	}
+
+	if startDate.Unix() < date.Unix() {
+		startDate = date
+	}
+
+	var timestamps []time.Time
+	for i := 0; i < 380; i++ {
+		nextDate, _ := relativeDate(date, rule, i)
+
+		//var curDate time.Time
+		//if len(timestamps) == 0 {
+		//	curDate = date
+		//} else {
+		//	curDate = timestamps[len(timestamps)-1]
+		//}
+
+		//if curDate.Unix() >= endDate.Unix() {
+		//	break
+		//}
+
+		//if curDate.Unix() == nextDate.Unix() {
+		//	break
+		//}
+
+		if nextDate.Unix() >= endDate.Unix() {
+			break
+		}
+
+		if nextDate.Unix() >= startDate.Unix() {
+			timestamps = append(timestamps, nextDate)
+		}
+	}
+
+	return timestamps, nil
 }
 
-func relativeDate(date time.Time, rule string) (time.Time, error) {
+func relativeDate(date time.Time, rule string, multiplier int) (time.Time, error) {
 	f, err := strconv.Atoi(rule[0 : len(rule)-1])
 	if err != nil {
 		return date, err
 	}
 	u := rule[len(rule)-1 : len(rule)]
 
-	if f == 0 {
-		return date, errors.New("Pattern doesn't repeat")
-	}
-
 	switch u {
 	case "d":
-		return date.AddDate(0, 0, 1*f), nil
+		date = date.AddDate(0, 0, (multiplier * 1 * f))
 	case "w":
-		return date.AddDate(0, 0, 7*f), nil
+		date = date.AddDate(0, 0, (multiplier * 1 * (f * 7)))
 	case "m":
-		return date.AddDate(0, 1, 0*f), nil
+		date = date.AddDate(0, (multiplier * 1 * f), 0)
 	}
 
-	return date, errors.New("Shouldn't have got here.")
+	return date, nil
 }
