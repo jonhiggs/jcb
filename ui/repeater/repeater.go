2437ec3a10315c78bbc7567e76d3d935a1bc2ae0
Jon Higgs <jhiggs@eml.cc>
2022-12-03 15:33:05 +1100

    Start a refactor of the repeater

--- ui/repeater/repeater.go
+++ ui/repeater/repeater.go
@@ -1,69 +1,55 @@
 package repeater
 
 import (
+	"errors"
 	dataf "jcb/ui/formatter/data"
+	"strings"
 	"time"
 )
 
 func Expand(date time.Time, startDate time.Time, endDate time.Time, rule string) ([]time.Time, error) {
-	var timestamps []time.Time
+	if strings.HasPrefix(rule, "0") {
+		if date.Unix() >= startDate.Unix() && date.Unix() < endDate.Unix() {
+			return []time.Time{date}, nil
+		} else {
+			return []time.Time{}, nil
+		}
+	} else {
+		timestamps := []time.Time{date}
+		for date.Unix() >= startDate.Unix() && date.Unix() < endDate.Unix() {
+			date, err := nextDate(date, rule)
+			if err != nil {
+				return timestamps, err
+			}
+			timestamps = append(timestamps, date)
+		}
+		return timestamps, nil
+	}
+}
 
+func nextDate(date time.Time, rule string) (time.Time, error) {
 	u, err := dataf.RepeatRuleUnit(rule)
 	if err != nil {
-		return timestamps, err
+		return date, err
 	}
 
 	f, err := dataf.RepeatRuleFrequency(rule)
 	if err != nil {
-		return timestamps, err
+		return date, err
+	}
+
+	if f == 0 {
+		return date, errors.New("Pattern doesn't repeat")
 	}
 
 	switch u {
 	case "d":
-		for i := 0; true; i += f {
-			var ts time.Time
-			ts = date.AddDate(0, 0, i)
-
-			if ts.Unix() < startDate.Unix() {
-				continue
-			} else if ts.Unix() <= endDate.Unix() {
-				timestamps = append(timestamps, ts)
-			} else {
-				break
-			}
-
-			if f == 0 {
-				break
-			}
-		}
-
+		return date.AddDate(0, 0, 1), nil
 	case "w":
-		for i := 0; true; i += f * 7 {
-			var ts time.Time
-			ts = date.AddDate(0, 0, i)
-
-			if ts.Unix() < startDate.Unix() {
-				continue
-			} else if ts.Unix() <= endDate.Unix() {
-				timestamps = append(timestamps, ts)
-			} else {
-				break
-			}
-		}
+		return date.AddDate(0, 0, 7), nil
 	case "m":
-		for i := 0; true; i += f {
-			var ts time.Time
-			ts = date.AddDate(0, i, 0)
-
-			if ts.Unix() < startDate.Unix() {
-				continue
-			} else if ts.Unix() <= endDate.Unix() {
-				timestamps = append(timestamps, ts)
-			} else {
-				break
-			}
-		}
+		return date.AddDate(0, 1, 0), nil
 	}
 
-	return timestamps, nil
+	return date, errors.New("Shouldn't have got here.")
 }
