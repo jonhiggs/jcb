e49d1345406ead16f7789873da46719aea6d60e3
Jon Higgs <jhiggs@eml.cc>
2022-12-05 06:57:04 +1100

    Add a failing test

--- lib/transaction/transaction.go
+++ lib/transaction/transaction.go
@@ -8,6 +8,12 @@ import (
 	"time"
 )
 
+type balance struct {
+	Id      int64
+	Cents   int64
+	Balance int64
+}
+
 func Find(id int64) (domain.Transaction, error) {
 	return db.FindTransaction(id)
 }
@@ -36,19 +42,18 @@ func Committed() ([]domain.Transaction, error) {
 	return db.CommittedTransactions()
 }
 
-func Commit(id int64, balance int64) error {
-	tset, bset, err := commitSet(id, balance)
+func Commit(id int64, initialBalance int64) error {
+	balance, err := commitSet(id, initialBalance)
 	if err != nil {
 		return err
 	}
 
-	for i, t := range tset {
-		err = db.CommitTransaction(t.Id, bset[i])
+	for _, b := range balance {
+		err = db.CommitTransaction(b.Id, b.Balance)
 		if err != nil {
 			return err
 		}
 	}
-
 	return nil
 }
 
@@ -65,12 +70,12 @@ func Balance(id int64) (int64, error) {
 }
 
 // set of transactions that need to be committed before committing provided id
-func commitSet(id int64, balance int64) ([]domain.Transaction, []int64, error) {
+func commitSet(id int64, initialBalance int64) ([]balance, error) {
 	var found bool
 
 	uncommitted, err := db.UncommittedTransactions()
 	if err != nil {
-		return []domain.Transaction{}, []int64{}, err
+		return []balance{}, err
 	}
 
 	var tset []domain.Transaction
@@ -84,18 +89,49 @@ func commitSet(id int64, balance int64) ([]domain.Transaction, []int64, error) {
 	}
 
 	if found {
-		return tset, balanceSet(tset, balance), nil
+		bset := balanceSet(tset, initialBalance)
+		errStr := fmt.Sprintln(bset)
+		return bset, errors.New(errStr)
+		return bset, nil
 	} else {
-		return []domain.Transaction{}, []int64{}, errors.New(fmt.Sprintf("No uncommitted transaction with id %d was found", id))
+		return []balance{}, errors.New(fmt.Sprintf("No uncommitted transaction with id %d was found", id))
 	}
 }
 
-func balanceSet(tset []domain.Transaction, finalBalance int64) []int64 {
-	bset := make([]int64, len(tset))
-	bset[len(tset)-1] = finalBalance
-	for i := len(tset) - 2; i >= 0; i-- {
-		bset[i] = bset[i+1] - tset[i].Cents
+func balanceSet(tset []domain.Transaction, initialBalance int64) []balance {
+	bset := make([]balance, len(tset))
+	bset[len(tset)-1].Balance = initialBalance
+	bset[len(tset)-1].Cents = tset[len(tset)-1].Cents
+	bset[len(tset)-1].Id = tset[len(tset)-1].Id
+
+	for i := len(bset) - 1; i > 0; i-- {
+		bset[i-1].Id = tset[i-1].Id
+		bset[i-1].Cents = tset[i-1].Cents
+		bset[i-1].Balance = bset[i].Cents + tset[i].Cents
 	}
+
+	//startingBalance := finalBalance
+	//for _, t := range tset {
+	//	startingBalance += t.Cents
+	//}
+	//for i, t := range tset {
+	//	if i == 0 {
+	//		//bset[i] = startingBalance
+	//		log.Printf("[%d]: %d", i, bset[i])
+	//	} else if i == len(tset) {
+	//		i[len(tset)] = finalBalance
+	//	} else {
+	//		bset[i-1] = bset[i-1] - t.Cents
+	//		log.Printf("[%d]: %d - %d = %d", i-1, bset[i-1], t.Cents, bset[i])
+	//	}
+	//}
+
+	//bset[len(tset)-1] = finalBalance
+	////log.Printf("setting balance to %d", bset[len(tset)-1])
+	//for i := len(tset) - 2; i >= 0; i-- {
+	//	bset[i] = bset[i+1] + tset[i].Cents
+	//	//log.Printf("[%d]: %d + %d = %d", i, bset[i+1], tset[i].Cents, bset[i])
+	//}
 
 	return bset
 }
--- lib/transaction/transaction_test.go
+++ lib/transaction/transaction_test.go
@@ -9,32 +9,56 @@ import (
 
 func TestBalanceSetA(t *testing.T) {
 	set := make([]domain.Transaction, 1)
-	set[0] = domain.Transaction{1, time.Now(), "one", 100}
+	set[0] = domain.Transaction{1, time.Now(), "one", 123}
 
 	got := balanceSet(set, 100)
 
-	if got[0] != 100 {
-		t.Error(fmt.Sprintf("expected 100 but got %d", got[0]))
+	if got[0].Id != 1 {
+		t.Error(fmt.Sprintf("expected id 1 but got %d", got[0].Id))
+	}
+
+	if got[0].Cents != 123 {
+		t.Error(fmt.Sprintf("expected cents of 123, got %d", got[0].Cents))
+	}
+
+	if got[0].Balance != 100 {
+		t.Error(fmt.Sprintf("expected balance of 100, got %d", got[0].Balance))
 	}
 
 	if len(got) != 1 {
-		t.Error(fmt.Sprintf("expected 1 element but got %d", len(got)))
+		t.Error(fmt.Sprintf("expected 1 element, got %d", len(got)))
 	}
 }
 
 func TestBalanceSetB(t *testing.T) {
 	set := make([]domain.Transaction, 2)
-	set[0] = domain.Transaction{1, time.Now(), "one", 100}
-	set[1] = domain.Transaction{1, time.Now(), "two", 100}
+	set[0] = domain.Transaction{0, time.Now(), "one", -100}
+	set[1] = domain.Transaction{1, time.Now(), "two", -100}
 
 	got := balanceSet(set, 100)
 
-	if got[0] != 0 {
-		t.Error(fmt.Sprintf("expected 0 but got %d", got[0]))
+	if got[0].Id != 0 {
+		t.Error(fmt.Sprintf("expected id 1 but got %d", got[0].Id))
 	}
 
-	if got[1] != 100 {
-		t.Error(fmt.Sprintf("expected 100 but got %d", got[1]))
+	if got[0].Cents != -100 {
+		t.Error(fmt.Sprintf("expected cents of -100, got %d", got[0].Cents))
+	}
+
+	if got[0].Balance != 0 {
+		t.Error(fmt.Sprintf("expected balance of 0, got %d", got[0].Balance))
+	}
+
+	if got[1].Id != 1 {
+		t.Error(fmt.Sprintf("expected id 1 but got %d", got[1].Id))
+	}
+
+	if got[1].Cents != -100 {
+		t.Error(fmt.Sprintf("expected cents of -100, got %d", got[1].Cents))
+	}
+
+	if got[1].Balance != 100 {
+		t.Error(fmt.Sprintf("expected balance of 100, got %d", got[1].Balance))
 	}
 
 	if len(got) != 2 {
@@ -42,63 +66,63 @@ func TestBalanceSetB(t *testing.T) {
 	}
 }
 
-func TestBalanceSetC(t *testing.T) {
-	set := make([]domain.Transaction, 3)
-	set[0] = domain.Transaction{1, time.Now(), "one", -80}
-	set[1] = domain.Transaction{1, time.Now(), "two", -100}
-	set[2] = domain.Transaction{1, time.Now(), "three", 1000}
-
-	got := balanceSet(set, 1000)
-
-	if got[0] != 1180 {
-		t.Error(fmt.Sprintf("expected 1180 but got %d", got[0]))
-	}
-
-	if got[1] != 1100 {
-		t.Error(fmt.Sprintf("expected 1100 but got %d", got[1]))
-	}
-
-	if got[2] != 1000 {
-		t.Error(fmt.Sprintf("expected 1000 but got %d", got[2]))
-	}
-
-	if len(got) != 3 {
-		t.Error(fmt.Sprintf("expected 1 element but got %d", len(got)))
-	}
-}
-
-func TestBalanceSetD(t *testing.T) {
-	set := make([]domain.Transaction, 6)
-	set[0] = domain.Transaction{1, time.Now(), "one", 20}
-	set[1] = domain.Transaction{1, time.Now(), "two", 20}
-	set[2] = domain.Transaction{1, time.Now(), "three", 20}
-	set[3] = domain.Transaction{1, time.Now(), "one", 10}
-	set[4] = domain.Transaction{1, time.Now(), "two", 10}
-	set[5] = domain.Transaction{1, time.Now(), "three", 10}
-
-	got := balanceSet(set, 100)
-
-	if got[0] != 30 {
-		t.Error(fmt.Sprintf("expected 30 but got %d", got[0]))
-	}
-
-	if got[1] != 50 {
-		t.Error(fmt.Sprintf("expected 50 but got %d", got[1]))
-	}
-
-	if got[2] != 70 {
-		t.Error(fmt.Sprintf("expected 70 but got %d", got[2]))
-	}
-
-	if got[3] != 80 {
-		t.Error(fmt.Sprintf("expected 80 but got %d", got[3]))
-	}
-
-	if got[4] != 90 {
-		t.Error(fmt.Sprintf("expected 90 but got %d", got[4]))
-	}
-
-	if got[5] != 100 {
-		t.Error(fmt.Sprintf("expected 100 but got %d", got[5]))
-	}
-}
+//func TestBalanceSetC(t *testing.T) {
+//	set := make([]domain.Transaction, 3)
+//	set[0] = domain.Transaction{1, time.Now(), "one", -1343}
+//	set[1] = domain.Transaction{1, time.Now(), "two", -80}
+//	set[2] = domain.Transaction{1, time.Now(), "three", -100}
+//
+//	got := balanceSet(set, 1000)
+//
+//	if got[0] != 820 {
+//		t.Error(fmt.Sprintf("expected 820 but got %d", got[0]))
+//	}
+//
+//	if got[1] != 900 {
+//		t.Error(fmt.Sprintf("expected 900 but got %d", got[1]))
+//	}
+//
+//	if got[2] != 1000 {
+//		t.Error(fmt.Sprintf("expected 1000 but got %d", got[2]))
+//	}
+//
+//	if len(got) != 3 {
+//		t.Error(fmt.Sprintf("expected 1 element but got %d", len(got)))
+//	}
+//}
+//
+//func TestBalanceSetD(t *testing.T) {
+//	set := make([]domain.Transaction, 6)
+//	set[0] = domain.Transaction{1, time.Now(), "one", -20}
+//	set[1] = domain.Transaction{1, time.Now(), "two", -20}
+//	set[2] = domain.Transaction{1, time.Now(), "three", -20}
+//	set[3] = domain.Transaction{1, time.Now(), "one", -10}
+//	set[4] = domain.Transaction{1, time.Now(), "two", -10}
+//	set[5] = domain.Transaction{1, time.Now(), "three", -10}
+//
+//	got := balanceSet(set, 100)
+//
+//	if got[0] != 30 {
+//		t.Error(fmt.Sprintf("expected 30 but got %d", got[0]))
+//	}
+//
+//	if got[1] != 50 {
+//		t.Error(fmt.Sprintf("expected 50 but got %d", got[1]))
+//	}
+//
+//	if got[2] != 70 {
+//		t.Error(fmt.Sprintf("expected 70 but got %d", got[2]))
+//	}
+//
+//	if got[3] != 80 {
+//		t.Error(fmt.Sprintf("expected 80 but got %d", got[3]))
+//	}
+//
+//	if got[4] != 90 {
+//		t.Error(fmt.Sprintf("expected 90 but got %d", got[4]))
+//	}
+//
+//	if got[5] != 100 {
+//		t.Error(fmt.Sprintf("expected 100 but got %d", got[5]))
+//	}
+//}
