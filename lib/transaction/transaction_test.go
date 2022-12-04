cea2c746d233155817fb3bc1720f91d8ee65a857
Jon Higgs <jhiggs@eml.cc>
2022-12-04 18:48:10 +1100

    Add a test

--- lib/transaction/transaction_test.go
+++ lib/transaction/transaction_test.go
@@ -66,3 +66,39 @@ func TestBalanceSetC(t *testing.T) {
 		t.Error(fmt.Sprintf("expected 1 element but got %d", len(got)))
 	}
 }
+
+func TestBalanceSetD(t *testing.T) {
+	set := make([]domain.Transaction, 6)
+	set[0] = domain.Transaction{1, time.Now(), "one", 20}
+	set[1] = domain.Transaction{1, time.Now(), "two", 20}
+	set[2] = domain.Transaction{1, time.Now(), "three", 20}
+	set[3] = domain.Transaction{1, time.Now(), "one", 10}
+	set[4] = domain.Transaction{1, time.Now(), "two", 10}
+	set[5] = domain.Transaction{1, time.Now(), "three", 10}
+
+	got := balanceSet(set, 100)
+
+	if got[0] != 30 {
+		t.Error(fmt.Sprintf("expected 30 but got %d", got[0]))
+	}
+
+	if got[1] != 50 {
+		t.Error(fmt.Sprintf("expected 50 but got %d", got[1]))
+	}
+
+	if got[2] != 70 {
+		t.Error(fmt.Sprintf("expected 70 but got %d", got[2]))
+	}
+
+	if got[3] != 80 {
+		t.Error(fmt.Sprintf("expected 80 but got %d", got[3]))
+	}
+
+	if got[4] != 90 {
+		t.Error(fmt.Sprintf("expected 90 but got %d", got[4]))
+	}
+
+	if got[5] != 100 {
+		t.Error(fmt.Sprintf("expected 100 but got %d", got[5]))
+	}
+}
