f42cb0f0b8bcd7743195dc7308ef8bbfb730bd47
Jon Higgs <jhiggs@eml.cc>
2022-12-07 06:11:24 +1100

    Ensure that database holds an opening balance

--- db/db.go
+++ db/db.go
@@ -2,16 +2,18 @@ package db
 
 import (
 	"database/sql"
+	"jcb/domain"
 	"log"
 	"os"
 	"path/filepath"
+	"time"
 
 	_ "github.com/mattn/go-sqlite3"
 )
 
 var db *sql.DB
 
-func Init(file string) error {
+func Init(file string, year int) error {
 	var err error
 	makeConfigDir(file)
 	db, err = sql.Open("sqlite3", file)
@@ -27,10 +29,23 @@ func Init(file string) error {
 			description TEXT,
 			cents INTEGER,
 			balance INTEGER,
-			committedAt TEXT
+			committedAt TEXT,
+			UNIQUE(id)
 	    );
 	`
 	_, err = db.Exec(sts)
+
+	statement, err := db.Prepare("INSERT OR IGNORE INTO transactions (id, date, description, cents) VALUES (?, ?, ?, ?)")
+	if err != nil {
+		return err
+	}
+
+	t := domain.Transaction{0, time.Date(year, 1, 1, 0, 0, 0, 0, time.UTC), "Opening Balance", 0}
+	_, err = statement.Exec(t.Id, t.Date, t.Description, t.Cents)
+	if err != nil {
+		return err
+	}
+
 	return err
 }
 
