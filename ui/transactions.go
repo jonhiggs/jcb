7f05e1c9103cd06b72855c5fc33a2bac95f69aba
Jon Higgs <jhiggs@eml.cc>
2022-12-15 18:35:58 +1100

    Move transaction functions into their own file

---
+++ ui/transactions.go
@@ -0,0 +1,128 @@
+package ui
+
+import (
+	stringf "jcb/lib/formatter/string"
+	"jcb/lib/transaction"
+
+	"code.rocketnine.space/tslocum/cview"
+	"github.com/gdamore/tcell/v2"
+)
+
+func createTransactionsTable() *cview.Table {
+	year := 2022
+	table := cview.NewTable()
+	table.Select(0, 0)
+	table.SetBorders(false)
+	table.SetFixed(1, 1)
+	table.SetSelectable(true, false)
+	table.SetSeparator(' ')
+	table.SetRect(0, 0, 72, 20)
+
+	committed, _ := transaction.Committed(year)
+	uncommitted, _ := transaction.Uncommitted(year)
+	all := committed
+	for _, t := range uncommitted {
+		all = append(all, t)
+	}
+
+	cell := cview.NewTableCell("DATE")
+	cell.SetTextColor(tcell.ColorYellow)
+	cell.SetAttributes(tcell.AttrUnderline | tcell.AttrBold)
+	cell.SetSelectable(false)
+	cell.SetAlign(cview.AlignLeft)
+	table.SetCell(0, 0, cell)
+
+	cell = cview.NewTableCell("DESCRIPTION")
+	cell.SetTextColor(tcell.ColorYellow)
+	cell.SetAttributes(tcell.AttrUnderline | tcell.AttrBold)
+	cell.SetSelectable(false)
+	cell.SetAlign(cview.AlignLeft)
+	table.SetCell(0, 1, cell)
+
+	cell = cview.NewTableCell("AMOUNT")
+	cell.SetTextColor(tcell.ColorYellow)
+	cell.SetAttributes(tcell.AttrUnderline | tcell.AttrBold)
+	cell.SetSelectable(false)
+	cell.SetAlign(cview.AlignRight)
+	table.SetCell(0, 2, cell)
+
+	cell = cview.NewTableCell("BALANCE")
+	cell.SetTextColor(tcell.ColorYellow)
+	cell.SetAttributes(tcell.AttrUnderline | tcell.AttrBold)
+	cell.SetSelectable(false)
+	cell.SetAlign(cview.AlignRight)
+	table.SetCell(0, 3, cell)
+
+	b := int64(0)
+	for i, t := range all {
+		b += t.Cents
+		date, _ := stringf.Date(t.Date)
+		description, _ := stringf.Description(t.Description)
+		cents, _ := stringf.Cents(t.Cents)
+		balance, _ := stringf.Cents(b)
+		isCommitted := false
+
+		for _, ct := range committed {
+			if ct.Id == t.Id {
+				isCommitted = true
+			}
+		}
+
+		var color tcell.Color
+		var attributes tcell.AttrMask
+		if b < 0 {
+			color = tcell.ColorRed
+		} else if isCommitted {
+			color = tcell.ColorWhite
+			attributes = 0
+		} else {
+			color = tcell.ColorBlue
+			attributes = tcell.AttrBold
+		}
+
+		cell = cview.NewTableCell(date)
+		cell.SetTextColor(color)
+		cell.SetAttributes(attributes)
+		cell.SetAlign(cview.AlignLeft)
+		table.SetCell(i+1, 0, cell)
+
+		cell = cview.NewTableCell(description)
+		cell.SetTextColor(color)
+		cell.SetAttributes(attributes)
+		cell.SetAlign(cview.AlignLeft)
+		table.SetCell(i+1, 1, cell)
+
+		cell = cview.NewTableCell(cents)
+		cell.SetTextColor(color)
+		cell.SetAttributes(attributes)
+		cell.SetAlign(cview.AlignRight)
+		table.SetCell(i+1, 2, cell)
+
+		cell = cview.NewTableCell(balance)
+		cell.SetTextColor(color)
+		cell.SetAttributes(attributes)
+		cell.SetAlign(cview.AlignRight)
+		table.SetCell(i+1, 3, cell)
+	}
+
+	table.SetDoneFunc(func(key tcell.Key) {
+		if key == tcell.KeyEscape {
+			app.Stop()
+		}
+
+		if key == tcell.KeyEscape {
+			app.Stop()
+		}
+		if key == tcell.KeyEnter {
+			table.SetSelectable(true, true)
+		}
+	})
+
+	table.SetSelectedFunc(func(row int, column int) {
+		panels.ShowPanel("insert")
+		//table.GetCell(row, column).SetTextColor(tcell.ColorRed.TrueColor())
+		//table.SetSelectable(false, false)
+	})
+
+	return table
+}
--- ui/ui.go
+++ ui/ui.go
@@ -1,14 +1,11 @@
 package ui
 
 import (
-	stringf "jcb/lib/formatter/string"
-	"jcb/lib/transaction"
 	"log"
 	"time"
 
 	"code.rocketnine.space/tslocum/cbind"
 	"code.rocketnine.space/tslocum/cview"
-	"github.com/gdamore/tcell/v2"
 )
 
 var app *cview.Application
@@ -18,9 +15,6 @@ var lowestBalanceDate time.Time
 
 func Start(year int) {
 	app = cview.NewApplication()
-
-	table := TransactionMenu(year)
-	table.SetRect(0, 0, 72, 20)
 
 	box0 := cview.NewTextView()
 	box0.SetDynamicColors(true)
@@ -37,7 +31,7 @@ func Start(year int) {
 	balance.SetTextAlign(cview.AlignRight)
 
 	panels = cview.NewPanels()
-	panels.AddPanel("balance", table, false, true)
+	panels.AddPanel("transactions", createTransactionsTable(), false, true)
 	panels.AddPanel("insert", createInsertForm(), false, false)
 
 	status := cview.NewTextView()
@@ -52,24 +46,6 @@ func Start(year int) {
 	grid.AddItem(status, 2, 0, 1, 1, 0, 0, false)
 	grid.AddItem(balance, 2, 1, 1, 1, 0, 0, false)
 
-	table.SetDoneFunc(func(key tcell.Key) {
-		if key == tcell.KeyEscape {
-			app.Stop()
-		}
-
-		if key == tcell.KeyEscape {
-			app.Stop()
-		}
-		if key == tcell.KeyEnter {
-			table.SetSelectable(true, true)
-		}
-	})
-	table.SetSelectedFunc(func(row int, column int) {
-		panels.ShowPanel("insert")
-		//table.GetCell(row, column).SetTextColor(tcell.ColorRed.TrueColor())
-		//table.SetSelectable(false, false)
-	})
-
 	c := cbind.NewConfiguration()
 
 	if err := c.Set("i", handleOpenInsert); err != nil {
@@ -79,138 +55,4 @@ func Start(year int) {
 	app.SetInputCapture(c.Capture)
 	app.SetRoot(grid, true)
 	app.Run()
-}
-
-func TransactionMenu(year int) *cview.Table {
-	table := cview.NewTable()
-	table.Select(0, 0)
-	table.SetBorders(false)
-	table.SetFixed(1, 1)
-	table.SetSelectable(true, false)
-	table.SetSeparator(' ')
-
-	committed, _ := transaction.Committed(year)
-	uncommitted, _ := transaction.Uncommitted(year)
-	all := committed
-	for _, t := range uncommitted {
-		all = append(all, t)
-	}
-
-	cell := cview.NewTableCell("DATE")
-	cell.SetTextColor(tcell.ColorYellow)
-	cell.SetAttributes(tcell.AttrUnderline | tcell.AttrBold)
-	cell.SetSelectable(false)
-	cell.SetAlign(cview.AlignLeft)
-	table.SetCell(0, 0, cell)
-
-	cell = cview.NewTableCell("DESCRIPTION")
-	cell.SetTextColor(tcell.ColorYellow)
-	cell.SetAttributes(tcell.AttrUnderline | tcell.AttrBold)
-	cell.SetSelectable(false)
-	cell.SetAlign(cview.AlignLeft)
-	table.SetCell(0, 1, cell)
-
-	cell = cview.NewTableCell("AMOUNT")
-	cell.SetTextColor(tcell.ColorYellow)
-	cell.SetAttributes(tcell.AttrUnderline | tcell.AttrBold)
-	cell.SetSelectable(false)
-	cell.SetAlign(cview.AlignRight)
-	table.SetCell(0, 2, cell)
-
-	cell = cview.NewTableCell("BALANCE")
-	cell.SetTextColor(tcell.ColorYellow)
-	cell.SetAttributes(tcell.AttrUnderline | tcell.AttrBold)
-	cell.SetSelectable(false)
-	cell.SetAlign(cview.AlignRight)
-	table.SetCell(0, 3, cell)
-
-	b := int64(0)
-	for i, t := range all {
-		b += t.Cents
-		date, _ := stringf.Date(t.Date)
-		description, _ := stringf.Description(t.Description)
-		cents, _ := stringf.Cents(t.Cents)
-		balance, _ := stringf.Cents(b)
-		isCommitted := false
-
-		for _, ct := range committed {
-			if ct.Id == t.Id {
-				isCommitted = true
-			}
-		}
-
-		var color tcell.Color
-		var attributes tcell.AttrMask
-		if b < 0 {
-			color = tcell.ColorRed
-		} else if isCommitted {
-			color = tcell.ColorWhite
-			attributes = 0
-		} else {
-			color = tcell.ColorBlue
-			attributes = tcell.AttrBold
-		}
-
-		cell = cview.NewTableCell(date)
-		cell.SetTextColor(color)
-		cell.SetAttributes(attributes)
-		cell.SetAlign(cview.AlignLeft)
-		table.SetCell(i+1, 0, cell)
-
-		cell = cview.NewTableCell(description)
-		cell.SetTextColor(color)
-		cell.SetAttributes(attributes)
-		cell.SetAlign(cview.AlignLeft)
-		table.SetCell(i+1, 1, cell)
-
-		cell = cview.NewTableCell(cents)
-		cell.SetTextColor(color)
-		cell.SetAttributes(attributes)
-		cell.SetAlign(cview.AlignRight)
-		table.SetCell(i+1, 2, cell)
-
-		cell = cview.NewTableCell(balance)
-		cell.SetTextColor(color)
-		cell.SetAttributes(attributes)
-		cell.SetAlign(cview.AlignRight)
-		table.SetCell(i+1, 3, cell)
-	}
-
-	return table
-}
-
-func Window() *cview.WindowManager {
-	list := cview.NewList()
-	list.ShowSecondaryText(false)
-	list.AddItem(cview.NewListItem("Item #1"))
-	list.AddItem(cview.NewListItem("Item #2"))
-	list.AddItem(cview.NewListItem("Item #3"))
-	list.AddItem(cview.NewListItem("Item #4"))
-	list.AddItem(cview.NewListItem("Item #5"))
-	list.AddItem(cview.NewListItem("Item #6"))
-	list.AddItem(cview.NewListItem("Item #7"))
-
-	loremIpsum := cview.NewTextView()
-	loremIpsum.SetText("balh blah blha")
-
-	wm := cview.NewWindowManager()
-	w1 := cview.NewWindow(list)
-	w1.SetRect(2, 2, 10, 7)
-	w2 := cview.NewWindow(loremIpsum)
-	w2.SetRect(7, 4, 12, 12)
-	w1.SetTitle("List")
-	w2.SetTitle("Lorem Ipsum")
-	wm.Add(w1, w2)
-
-	return wm
-}
-
-func Box() *cview.Box {
-	box := cview.NewBox()
-	box.SetBorder(true)
-	box.SetBorderAttributes(tcell.AttrBold)
-	box.SetRect(10, 10, 20, 20)
-	box.SetTitle("New Transaction")
-
-	return box
 }
