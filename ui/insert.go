a922c5155a218529205d81125639beb42b85cff8
Jon Higgs <jhiggs@eml.cc>
2022-12-15 15:57:49 +1100

    Reset the insert form when leaving panel

--- ui/insert.go
+++ ui/insert.go
@@ -7,6 +7,10 @@ import (
 
 var insertForm *cview.Form
 var insertInputFieldDate *cview.InputField
+var insertInputFieldDescription *cview.InputField
+var insertInputFieldAmount *cview.InputField
+var insertInputFieldRepeatRule *cview.InputField
+var insertInputFieldRepeatUntil *cview.InputField
 
 func handleOpenInsert(ev *tcell.EventKey) *tcell.EventKey {
 	panels.ShowPanel("insert")
@@ -16,6 +20,10 @@ func handleOpenInsert(ev *tcell.EventKey) *tcell.EventKey {
 func handleCloseInsert() {
 	panels.HidePanel("insert")
 	insertInputFieldDate.SetText("")
+	insertInputFieldDescription.SetText("")
+	insertInputFieldAmount.SetText("")
+	insertInputFieldRepeatRule.SetText("0d")
+	insertInputFieldRepeatUntil.SetText("2022-12-31")
 	insertForm.SetFocus(0)
 	return
 }
@@ -24,15 +32,34 @@ func createInsertForm() *cview.Form {
 	insertForm = cview.NewForm()
 
 	insertInputFieldDate = cview.NewInputField()
-	insertInputFieldDate.SetLabel("Date")
+	insertInputFieldDate.SetLabel("Date:")
 	insertInputFieldDate.SetFieldWidth(11)
 	insertInputFieldDate.SetFieldNote("Your complete address")
 
+	insertInputFieldDescription = cview.NewInputField()
+	insertInputFieldDescription.SetLabel("Description:")
+	insertInputFieldDescription.SetFieldWidth(6)
+
+	insertInputFieldAmount = cview.NewInputField()
+	insertInputFieldAmount.SetLabel("Amount:")
+	insertInputFieldAmount.SetFieldWidth(6)
+
+	insertInputFieldRepeatRule = cview.NewInputField()
+	insertInputFieldRepeatRule.SetLabel("Repeat Every:")
+	insertInputFieldRepeatRule.SetFieldWidth(4)
+	insertInputFieldRepeatRule.SetText("0d")
+
+	insertInputFieldRepeatUntil = cview.NewInputField()
+	insertInputFieldRepeatUntil.SetLabel("Repeat Until:")
+	insertInputFieldRepeatUntil.SetFieldWidth(11)
+	insertInputFieldRepeatUntil.SetText("2022-12-31")
+
 	insertForm.AddFormItem(insertInputFieldDate)
-	insertForm.AddInputField("Description:", "", 0, nil, nil)
-	insertForm.AddInputField("Amount:", "", 6, nil, nil)
-	insertForm.AddInputField("Repeat Every:", "", 4, nil, nil)
-	insertForm.AddInputField("Repeat Until", "2022-12-31", 11, func(t string, c rune) bool { return true }, nil)
+	insertForm.AddFormItem(insertInputFieldDescription)
+	insertForm.AddFormItem(insertInputFieldAmount)
+	insertForm.AddFormItem(insertInputFieldRepeatRule)
+	insertForm.AddFormItem(insertInputFieldRepeatUntil)
+
 	insertForm.AddButton("Save", func() {})
 	insertForm.AddButton("Quit", handleCloseInsert)
 	insertForm.SetBorder(true)
