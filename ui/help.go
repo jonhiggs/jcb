0442864572fa67ba59f707b681e6315d4255c4fc
Jon Higgs <jhiggs@eml.cc>
2022-12-21 07:59:38 +1100

    Adjust the help page

--- ui/help.go
+++ ui/help.go
@@ -39,45 +39,45 @@ func createHelp() *cview.TextView {
 		`
  Commands:
 
-	h help  Print this help
-	q quit  Quit
-	w       Write changes to disk
-	wq      Write changes and quit
-	q!      Quit without writing
-	version Show version
+	h, help    Print this help
+	q, quit    Quit
+	w          Write changes to disk
+	wq         Write changes and quit
+	q!         Quit without writing
+	version    Show version
 
 
  Key Bindings:
 
-	i       Insert new transaction
-	x       Delete selected transaction
-	<enter> Edit selected transaction
-	j       Select next transaction
-	k       Select previous transaction
-	d       Scroll half a page down
-	u       Scroll half a page up
-	0       Select oldest uncommmitted transaction
-	*       Select similar transaction
-	}       Scroll to next month
-	{       Scroll to previous month
-	C       Commit all transactions to selection
-	:       Enter command mode
-	/       Enter find next query
-	?       Enter find previous query
-	n       Next matching transaction
-	N       Previous matching transaction
+	i          Insert new transaction
+	x          Delete selected transaction
+	<enter>    Edit selected transaction
+	j          Select next transaction
+	k          Select previous transaction
+	d          Scroll half a page down
+	u          Scroll half a page up
+	0          Select oldest uncommmitted transaction
+	*          Select similar transaction
+	}          Scroll to next month
+	{          Scroll to previous month
+	C          Commit all transactions to selection
+	:          Enter command mode
+	/          Enter find next query
+	?          Enter find previous query
+	n          Next matching transaction
+	N          Previous matching transaction
 
 
  Input Field Key Bindings:
 
-	<enter> Submit form
-	<esc>   Cancel form
-	C-a     Start of field
-	C-e     End of field
-	C-w     Delete word backwards
-	C-u     Clear field
-	M-f     Forward word
-	M-b     Backwards word
+	<enter>    Submit form
+	<esc>, C-c Cancel form
+	C-a        Start of field
+	C-e        End of field
+	C-w        Delete word backwards
+	C-u        Clear field
+	M-f        Forward word
+	M-b        Backwards word
 `)
 	helpTextView.SetDoneFunc(func(key tcell.Key) { handleCloseHelp() })
 
