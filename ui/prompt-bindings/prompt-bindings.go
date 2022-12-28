baaaa85c4925e77e24296918d17df2cc004b8931
Jon Higgs <jhiggs@eml.cc>
2022-12-29 07:03:35 +1100

    Move bindings into new file

---
+++ ui/prompt-bindings/prompt-bindings.go
@@ -0,0 +1,34 @@
+package promptBindings
+
+import (
+	"strings"
+
+	"code.rocketnine.space/tslocum/cview"
+)
+
+func DeleteChar(field *cview.InputField) {
+	pos := field.GetCursorPosition()
+	text := field.GetText()
+
+	textSlice := strings.Split(text, "")
+
+	var newSlice []string
+	for i, l := range textSlice {
+		if i == pos {
+			continue
+		}
+
+		newSlice = append(newSlice, l)
+	}
+
+	field.SetText(strings.Join(newSlice, ""))
+	field.SetCursorPosition(pos)
+}
+
+func BackwardChar(field *cview.InputField) {
+	field.SetCursorPosition(pos - 1)
+}
+
+func ForwardChar(field *cview.InputField) {
+	field.SetCursorPosition(pos - 1)
+}
