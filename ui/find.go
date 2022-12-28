112b53a6bd9a74ad6186b6bd6d4e0b9566b4d0f5
Jon Higgs <jhiggs@eml.cc>
2022-12-28 16:27:19 +1100

    Use prompt for find

--- ui/find.go
+++ ui/find.go
@@ -5,74 +5,56 @@ import (
 	"jcb/config"
 	"strings"
 
-	"code.rocketnine.space/tslocum/cbind"
-	"code.rocketnine.space/tslocum/cview"
 	"github.com/gdamore/tcell/v2"
 )
 
-var findForm *cview.Form
-var findInputField *cview.InputField
 var findQuery string
 
-func handleOpenFind(ev *tcell.EventKey) *tcell.EventKey {
-	panels.ShowPanel("find")
-	panels.SendToFront("find")
-
-	var label string
-
-	c := cbind.NewConfiguration()
+func handleFind(ev *tcell.EventKey) *tcell.EventKey {
 	switch ev.Rune() {
 	case '/':
-		label = "/"
-		c.SetKey(0, tcell.KeyEnter, handleFindForwards)
-		findInputField.SetText("")
+		openPrompt("/", "", func(ev *tcell.EventKey) *tcell.EventKey {
+			panels.HidePanel("prompt")
+			findQuery = promptInputField.GetText()
+			selectNextMatch()
+			return nil
+		})
 	case '?':
-		label = "?"
-		c.SetKey(0, tcell.KeyEnter, handleFindBackwards)
-		findInputField.SetText("")
+		openPrompt("?", "", func(ev *tcell.EventKey) *tcell.EventKey {
+			panels.HidePanel("prompt")
+			findQuery = promptInputField.GetText()
+			selectPrevMatch()
+			return nil
+		})
 	case 'T':
-		label = "Tag matched transactions:"
-		findInputField.SetText(selectedDescription())
-		c.SetKey(0, tcell.KeyEnter, handleTagMatches)
+		openPrompt("Tag matched transactions:", selectedDescription(), func(ev *tcell.EventKey) *tcell.EventKey {
+			panels.HidePanel("prompt")
+			findQuery = promptInputField.GetText()
+			tagMatches()
+			return nil
+		})
 	}
 
-	findInputField.SetLabel(label)
-	findInputField.SetFieldWidth(config.MAX_WIDTH - len(label))
-	c.SetKey(tcell.ModCtrl, tcell.KeyCtrlD, handleInputFormCustomBindings)
-	c.SetKey(tcell.ModCtrl, tcell.KeyCtrlF, handleInputFormCustomBindings)
-	c.SetKey(tcell.ModCtrl, tcell.KeyCtrlB, handleInputFormCustomBindings)
-	findInputField.SetInputCapture(c.Capture)
-	findForm.SetFocus(0)
 	return nil
 }
 
-func handleCloseFind() {
-	panels.HidePanel("find")
-}
-
 func handleFindForwards(ev *tcell.EventKey) *tcell.EventKey {
-	findQuery = findInputField.GetText()
-	handleCloseFind()
-	handleSelectNextMatch(ev)
-
+	selectNextMatch()
 	return nil
 }
 
 func handleFindBackwards(ev *tcell.EventKey) *tcell.EventKey {
-	findQuery = findInputField.GetText()
-	handleCloseFind()
-	handleSelectPrevMatch(ev)
-
+	selectPrevMatch()
 	return nil
 }
 
-func handleSelectNextMatch(ev *tcell.EventKey) *tcell.EventKey {
+func selectNextMatch() {
 	curRow, _ := transactionsTable.GetSelection()
 
 	for i := curRow + 1; i != curRow; i++ {
 		if strings.Contains(strings.ToLower(transactionsTable.GetCell(i, config.DESCRIPTION_COLUMN).GetText()), strings.ToLower(findQuery)) {
 			transactionsTable.Select(i, 0)
-			return nil
+			return
 		}
 
 		if i == len(transactionIds) {
@@ -81,11 +63,9 @@ func handleSelectNextMatch(ev *tcell.EventKey) *tcell.EventKey {
 	}
 
 	printStatus("No matches found")
-
-	return nil
 }
 
-func handleSelectPrevMatch(ev *tcell.EventKey) *tcell.EventKey {
+func selectPrevMatch() {
 	curRow, _ := transactionsTable.GetSelection()
 
 	for i := curRow - 1; i != curRow; i-- {
@@ -99,12 +79,11 @@ func handleSelectPrevMatch(ev *tcell.EventKey) *tcell.EventKey {
 		}
 	}
 
-	return nil
+	printStatus("No matches found")
 }
 
-func handleTagMatches(ev *tcell.EventKey) *tcell.EventKey {
+func tagMatches() {
 	curRow, _ := transactionsTable.GetSelection()
-	findQuery = findInputField.GetText()
 
 	matchCount := 0
 
@@ -122,27 +101,5 @@ func handleTagMatches(ev *tcell.EventKey) *tcell.EventKey {
 	transactionsTable.Select(curRow, 0)
 
 	printStatus(fmt.Sprintf("Tagged %d transactions", matchCount))
-
 	updateTransactionsTable()
-	handleCloseFind()
-
-	return nil
-}
-
-func createFindForm() *cview.Form {
-	findForm = cview.NewForm()
-	findForm.SetBorder(false)
-	findForm.SetCancelFunc(handleCloseFind)
-	findForm.SetItemPadding(0)
-	findForm.SetPadding(0, 0, 0, 0)
-	findForm.SetLabelColor(tcell.ColorWhite)
-	findForm.SetFieldBackgroundColor(tcell.ColorBlack)
-	findForm.SetFieldBackgroundColorFocused(tcell.ColorBlack)
-
-	findInputField = cview.NewInputField()
-	findInputField.SetFieldWidth(24)
-
-	findForm.AddFormItem(findInputField)
-
-	return findForm
 }
