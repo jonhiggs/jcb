314fa5f1c7d6b4aa4cc50dcd17b5e91af4ecd304
Jon Higgs <jhiggs@eml.cc>
2022-12-30 07:33:24 +1100

    Fix importing from an export

--- cmd/main.go
+++ cmd/main.go
@@ -37,7 +37,7 @@ func main() {
 	file := config.DefaultFile()
 	tsvFile := ""
 	exportTsv := false
-	fmt.Printf("Loading file %s\n", file)
+	fmt.Fprintf(os.Stderr, "Loading file %s\n", file)
 
 	results, _, err := optparse.Parse(options, os.Args)
 	if err != nil {
