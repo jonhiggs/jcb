aeeadf6a5bcd7a2f928907bdd9eab4edcead4584
Jon Higgs <jhiggs@eml.cc>
2023-01-11 07:34:01 +1100

    Add some placeholder data

--- ui/budgets.go
+++ ui/budgets.go
@@ -2,10 +2,6 @@ package ui
 
 import (
 	"jcb/config"
-	"jcb/lib/category"
-	stringf "jcb/lib/formatter/string"
-	"jcb/lib/transaction"
-	"time"
 
 	"code.rocketnine.space/tslocum/cbind"
 	"code.rocketnine.space/tslocum/cview"
@@ -26,9 +22,8 @@ func createBudgetTable() *cview.Table {
 	budgetTable.SetSelectionChangedFunc(func(r int, c int) { closeStatus() })
 
 	c := cbind.NewConfiguration()
-	c.Set("q", handleCloseReport)
-	c.Set("j", handleReportSelectNext)
-	c.Set("k", handleReportSelectPrev)
+	c.Set("j", handleBudgetSelectNext)
+	c.Set("k", handleBudgetSelectPrev)
 	c.Set("1", handleOpenHelp)
 	c.Set("2", handleOpenTransactions)
 	c.Set("3", handleOpenBudget)
@@ -48,24 +43,15 @@ func createBudgetTable() *cview.Table {
 func updateBudgetTable() {
 	var cell *cview.TableCell
 
-	st, _ := transaction.Find(selectionId())
-	year := st.Date.Year()
+	//st, _ := transaction.Find(selectionId())
+	//year := st.Date.Year()
 
 	columns := []string{
+		"",
+		"DATE",
 		"CATEGORY",
-		"JAN",
-		"FEB",
-		"MAR",
-		"APR",
-		"MAY",
-		"JUN",
-		"JUL",
-		"AUG",
-		"SEP",
-		"OCT",
-		"NOV",
-		"DEC",
-		"TOTAL",
+		"BUDGET",
+		"REMAINING",
 	}
 
 	for i, c := range columns {
@@ -83,75 +69,105 @@ func updateBudgetTable() {
 		budgetTable.SetCell(0, i, cell)
 	}
 
-	for row, catName := range category.All() {
-		for col, _ := range columns {
-			if col == 0 {
-				cell = cview.NewTableCell(catName)
-				cell.SetTextColor(config.COLOR_TITLE_FG)
-			} else {
-				var startTime time.Time
-				var endTime time.Time
+	cell = cview.NewTableCell("Ccn+")
+	budgetTable.SetCell(1, 0, cell)
 
-				switch col {
-				case 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12:
-					startTime = time.Date(year, time.Month(col), 1, 0, 0, 0, 0, time.UTC)
-					endTime = startTime.AddDate(0, 1, 0)
-				case 13:
-					startTime = time.Date(year, 1, 1, 0, 0, 0, 0, time.UTC)
-					endTime = startTime.AddDate(1, 0, 0)
-				}
+	cell = cview.NewTableCell("2023-01-02")
+	budgetTable.SetCell(1, 1, cell)
 
-				cents := category.Sum(catName, startTime, endTime)
-				cell = cview.NewTableCell(" " + stringf.Cents(cents))
+	cell = cview.NewTableCell("shopping")
+	budgetTable.SetCell(1, 2, cell)
 
-				if col == 13 {
-					cell.SetAttributes(tcell.AttrBold)
-					cell.SetBackgroundColor(config.COLOR_LIGHT_BG)
-				}
+	cell = cview.NewTableCell("20.33")
+	budgetTable.SetCell(1, 3, cell)
 
-				cell.SetAlign(cview.AlignRight)
-			}
-			budgetTable.SetCell(row+1, col, cell)
-		}
-	}
+	cell = cview.NewTableCell("20.33")
+	budgetTable.SetCell(1, 4, cell)
 
-	row := budgetTable.GetRowCount()
+	cell = cview.NewTableCell("Ccn+")
+	budgetTable.SetCell(2, 0, cell)
 
-	// XXX: don't add new total rows for every update
-	if budgetTable.GetCell(row-1, 0).GetText() == "" {
-		row = row - 1
-	}
+	cell = cview.NewTableCell("2023-01-02")
+	budgetTable.SetCell(2, 1, cell)
 
-	for col, _ := range columns {
-		var startTime time.Time
-		var endTime time.Time
+	cell = cview.NewTableCell("health")
+	budgetTable.SetCell(2, 2, cell)
 
-		if col == 0 {
-			continue
-		}
+	cell = cview.NewTableCell("340.00")
+	budgetTable.SetCell(2, 3, cell)
 
-		switch col {
-		case 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12:
-			startTime = time.Date(year, time.Month(col), 1, 0, 0, 0, 0, time.UTC)
-			endTime = startTime.AddDate(0, 1, 0)
-		case 13:
-			startTime = time.Date(year, 1, 1, 0, 0, 0, 0, time.UTC)
-			endTime = startTime.AddDate(1, 0, 0)
-		}
+	cell = cview.NewTableCell("340.00")
+	budgetTable.SetCell(2, 4, cell)
 
-		cents := transaction.Sum(startTime, endTime)
-		cell = cview.NewTableCell(" " + stringf.Cents(cents))
-		cell.SetSelectable(false)
-		cell.SetAlign(cview.AlignRight)
-		cell.SetAttributes(tcell.AttrBold)
-		cell.SetBackgroundColor(config.COLOR_LIGHT_BG)
+	//for row, catName := range category.All() {
+	//	for col, _ := range columns {
+	//		if col == 0 {
+	//			cell = cview.NewTableCell(catName)
+	//			cell.SetTextColor(config.COLOR_TITLE_FG)
+	//		} else {
+	//			var startTime time.Time
+	//			var endTime time.Time
 
-		if cents < 0 {
-			cell.SetTextColor(config.COLOR_NEGATIVE_FG)
-		} else {
-			cell.SetTextColor(config.COLOR_POSITIVE_FG)
-		}
+	//			switch col {
+	//			case 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12:
+	//				startTime = time.Date(year, time.Month(col), 1, 0, 0, 0, 0, time.UTC)
+	//				endTime = startTime.AddDate(0, 1, 0)
+	//			case 13:
+	//				startTime = time.Date(year, 1, 1, 0, 0, 0, 0, time.UTC)
+	//				endTime = startTime.AddDate(1, 0, 0)
+	//			}
 
-		budgetTable.SetCell(row, col, cell)
-	}
+	//			cents := category.Sum(catName, startTime, endTime)
+	//			cell = cview.NewTableCell(" " + stringf.Cents(cents))
+
+	//			if col == 13 {
+	//				cell.SetAttributes(tcell.AttrBold)
+	//				cell.SetBackgroundColor(config.COLOR_LIGHT_BG)
+	//			}
+
+	//			cell.SetAlign(cview.AlignRight)
+	//		}
+	//		budgetTable.SetCell(row+1, col, cell)
+	//	}
+	//}
+
+	//row := budgetTable.GetRowCount()
+
+	//// XXX: don't add new total rows for every update
+	//if budgetTable.GetCell(row-1, 0).GetText() == "" {
+	//	row = row - 1
+	//}
+
+	//for col, _ := range columns {
+	//	var startTime time.Time
+	//	var endTime time.Time
+
+	//	if col == 0 {
+	//		continue
+	//	}
+
+	//	switch col {
+	//	case 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12:
+	//		startTime = time.Date(year, time.Month(col), 1, 0, 0, 0, 0, time.UTC)
+	//		endTime = startTime.AddDate(0, 1, 0)
+	//	case 13:
+	//		startTime = time.Date(year, 1, 1, 0, 0, 0, 0, time.UTC)
+	//		endTime = startTime.AddDate(1, 0, 0)
+	//	}
+
+	//	cents := transaction.Sum(startTime, endTime)
+	//	cell = cview.NewTableCell(" " + stringf.Cents(cents))
+	//	cell.SetSelectable(false)
+	//	cell.SetAlign(cview.AlignRight)
+	//	cell.SetAttributes(tcell.AttrBold)
+	//	cell.SetBackgroundColor(config.COLOR_LIGHT_BG)
+
+	//	if cents < 0 {
+	//		cell.SetTextColor(config.COLOR_NEGATIVE_FG)
+	//	} else {
+	//		cell.SetTextColor(config.COLOR_POSITIVE_FG)
+	//	}
+
+	//	budgetTable.SetCell(row, col, cell)
+	//}
 }
