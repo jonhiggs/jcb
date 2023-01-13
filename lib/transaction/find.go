ec487404315e64e74eed4d751ac9728209781bc6
Jon Higgs <jhiggs@eml.cc>
2023-01-14 08:42:51 +1100

    Break up the transactions functions

---
+++ lib/transaction/find.go
@@ -0,0 +1,172 @@
+// Functions to collect slices of transactions.
+package transaction
+
+import (
+	"database/sql"
+	"fmt"
+	"jcb/db"
+	"log"
+	"time"
+)
+
+// Returns every transaction within time range.
+func All(startTime time.Time, endTime time.Time) []*Transaction {
+	var rows *sql.Rows
+	var err error
+
+	var records []*Transaction
+
+	statement, err := db.Conn.Prepare(`
+		SELECT id, date, description, cents, notes, category
+		FROM (
+			SELECT id, date, description, cents, notes, category, IFNULL(committedAt, "2999") AS committedAt
+			FROM transactions
+			ORDER BY committedAt ASC, date ASC, cents DESC
+		)
+		WHERE date >= ? AND date <= ?
+	`)
+	if err != nil {
+		log.Fatal(fmt.Sprintf("All(): %s", err))
+	}
+
+	rows, err = statement.Query(startTime, endTime)
+	if err != nil {
+		log.Fatal(fmt.Sprintf("All(): %s", err))
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+		var id int64
+		var dateString string
+		var description string
+		var cents int64
+		var notes string
+		var category string
+
+		err = rows.Scan(
+			&id,
+			&dateString,
+			&description,
+			&cents,
+			&notes,
+			&category,
+		)
+
+		if err != nil {
+			log.Fatal(fmt.Sprintf("All(): %s", err))
+		}
+
+		t := new(Transaction)
+		t.id = id
+		t.SetDate(db.ParseDate(dateString))
+		t.SetDescription(description)
+		t.SetCents(cents)
+		t.SetNotes(notes)
+		t.SetCategory(category)
+
+		records = append(records, t)
+	}
+
+	return records
+}
+
+// Returns transactions within a category
+func FindByCategory(category string, start time.Time, end time.Time) []*Transaction {
+	var records []*Transaction
+
+	for _, t := range All(start, end) {
+		if t.GetCategory(false) == category {
+			records = append(records, t)
+		}
+	}
+
+	return records
+}
+
+// Return a transaction from a transaction ID.
+func Find(id int64) (*Transaction, error) {
+	var dateString string
+	var description string
+	var cents int64
+	var notes string
+	var category string
+
+	statement, _ := db.Conn.Prepare(`
+		SELECT id, date, description, cents, notes, category 
+		FROM transactions WHERE id = ?
+	`)
+
+	err := statement.QueryRow(id).Scan(&id, &dateString, &description, &cents, &notes, &category)
+	if err != nil {
+		log.Fatal(fmt.Sprintf("Find(): %s", err))
+	}
+
+	t := new(Transaction)
+	t.id = id
+	t.SetDate(db.ParseDate(dateString))
+	t.SetDescription(description)
+	t.SetCents(cents)
+	t.SetNotes(notes)
+	t.SetCategory(category)
+
+	return t, nil
+}
+
+func FindFirst() (*Transaction, error) {
+	var id int64
+	var dateString string
+	var description string
+	var cents int64
+	var notes string
+	var category string
+
+	statement, _ := db.Conn.Prepare(`
+		SELECT id, date, description, cents, notes, category 
+		FROM transactions ORDER BY date LIMIT 1
+	`)
+
+	err := statement.QueryRow().Scan(&id, &dateString, &description, &cents, &notes, &category)
+	if err != nil {
+		log.Fatal(fmt.Sprintf("FindFirst(): %s", err))
+	}
+
+	t := new(Transaction)
+	t.id = id
+	t.SetDate(db.ParseDate(dateString))
+	t.SetDescription(description)
+	t.SetCents(cents)
+	t.SetNotes(notes)
+	t.SetCategory(category)
+
+	return t, nil
+}
+
+func FindLast() (*Transaction, error) {
+	var id int64
+	var dateString string
+	var description string
+	var cents int64
+	var notes string
+	var category string
+
+	statement, _ := db.Conn.Prepare(`
+		SELECT id, date, description, cents, notes, category 
+		FROM transactions ORDER BY date DESC LIMIT 1
+	`)
+
+	err := statement.QueryRow().Scan(&id, &dateString, &description, &cents, &notes, &category)
+	if err != nil {
+		log.Fatal(fmt.Sprintf("FindLast(): %s", err))
+	}
+
+	t := new(Transaction)
+	t.id = id
+	t.SetDate(db.ParseDate(dateString))
+	t.SetDescription(description)
+	t.SetCents(cents)
+	t.SetNotes(notes)
+	t.SetCategory(category)
+
+	return t, nil
+}
---
+++ lib/transaction/functions.go
@@ -0,0 +1,123 @@
+package transaction
+
+import (
+	"errors"
+	"jcb/db"
+	"time"
+)
+
+// Update or Create a transaction in the Database.
+func (t *Transaction) Save() error {
+	if t.IsCommitted() {
+		return errors.New("Cannot modify committed transactions")
+	}
+
+	if t.id == -1 {
+		statement, err := db.Conn.Prepare(`
+			INSERT INTO transactions (
+				date,
+				description,
+				cents,
+				notes,
+				updatedAt,
+				category
+			) VALUES (?, ?, ?, ?, ?, ?)
+		`)
+		if err != nil {
+			return err
+		}
+
+		res, err := statement.Exec(t.date, t.description, t.cents, t.notes, db.TimeNow(), t.category)
+		if err != nil {
+			return err
+		}
+
+		t.id, _ = res.LastInsertId()
+	} else {
+		statement, err := db.Conn.Prepare(`
+			UPDATE transactions
+			SET
+				date = ?,
+				description = ?,
+				cents = ?,
+				notes = ?,
+				category = ?,
+				updatedAt = ?
+			WHERE id = ?
+				AND committedAt IS NULL
+		`)
+		if err != nil {
+			return err
+		}
+		_, err = statement.Exec(t.date, t.description, t.cents, t.notes, t.category, db.TimeNow(), t.id)
+
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// Delete a transaction
+func (t *Transaction) Delete() error {
+	if t.IsCommitted() {
+		return errors.New("Cannot delete committed transactions")
+	}
+	statement, err := db.Conn.Prepare(`
+		DELETE FROM transactions WHERE id = ? AND committedAt IS NULL
+	`)
+	if err != nil {
+		return err
+	}
+	_, err = statement.Exec(t.GetID())
+
+	db.Dirty = true
+
+	return err
+}
+
+// Commit a transaction
+func (t *Transaction) Commit() error {
+	return errors.New("Not implemented")
+	if !t.IsCommittable() {
+		return errors.New("Commit older transactions first")
+	}
+
+	//balance := CommittedBalance() + cents
+	//statement, _ := db.Prepare("UPDATE transactions SET balance = ?, committedAt = ?, updatedAt = ? WHERE id = ? AND committedAt IS NULL")
+
+	//_, err := statement.Exec(balance, timeNow(), timeNow(), id)
+	//if err != nil {
+	//	log.Fatal(fmt.Sprintf("Committransaction(): %s", err))
+	//}
+
+	return nil
+}
+
+// Uncommit a transaction
+func (t *Transaction) Uncommit() error {
+	return errors.New("Not implemented")
+	//balance := CommittedBalance() + cents
+	//statement, _ := db.Prepare("UPDATE transactions SET balance = ?, committedAt = ?, updatedAt = ? WHERE id = ? AND committedAt IS NULL")
+
+	//_, err := statement.Exec(balance, timeNow(), timeNow(), id)
+	//if err != nil {
+	//	log.Fatal(fmt.Sprintf("Committransaction(): %s", err))
+	//}
+
+	return nil
+}
+
+func DateRange() (time.Time, time.Time) {
+	first, _ := FindFirst()
+	last, _ := FindLast()
+	return first.GetDate(), last.GetDate()
+}
+
+func SumCents(ts []*Transaction) int64 {
+	var sum int64
+	for _, t := range ts {
+		sum += t.GetCents()
+	}
+	return sum
+}
